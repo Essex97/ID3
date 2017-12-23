5db17cb98244a5f2d47223bdfc7e8c5b81a0b0e0
Essex97 <stratosxen@gmail.com>
2017-12-23 22:21:10 +0200

    level1

--- src/Car.java
+++ src/Car.java
@@ -1,11 +1,8 @@
-import java.io.*;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.List;
 
 public class Car {
 
-    private String bying;
+    private String buying;
     private String maint;
     private String doors;
     private String persons;
@@ -13,8 +10,8 @@ public class Car {
     private String safety;
     private String category;
 
-    public Car(String bying, String maint, String doors, String persons, String lug_boot, String safety, String category) {
-        this.bying = bying;
+    public Car(String buying, String maint, String doors, String persons, String lug_boot, String safety, String category) {
+        this.buying = buying;
         this.maint = maint;
         this.doors = doors;
         this.persons = persons;
@@ -22,9 +19,10 @@ public class Car {
         this.safety = safety;
         this.category = category;
     }
-//-----------------Getters-----------------//
-    public String getBying() {
-        return bying;
+
+    //-----------------Getters-----------------//
+    public String getBuying() {
+        return buying;
     }
 
     public String getMaint() {
@@ -51,123 +49,22 @@ public class Car {
         return category;
     }
 
-    public void readFile(String data)
-    {
-        File f = null;
-        BufferedReader reader = null;
-        String line;
-        String[] splited;
-        ArrayList<Car> Cars = new ArrayList<Car>();
-
-        try {
-            f = new File(data);
-        } catch (NullPointerException e) {
-            System.err.println("Error occured while opening files...");
+    /*public String getValueOf(String attribute){
+        if(attribute.equals("buying"))
+            return getBuying();
+        else if(attribute.equals("maint"))
+            return getMaint();
+        else if(attribute.equals("doors"))
+            return getDoors();
+        else if(attribute.equals("persons"))
+            return getPersons();
+        else if(attribute.equals("lug_boot"))
+            return getLug_boot();
+        else if(attribute.equals("safety")){
+            return getSafety();
         }
-
-        try {
-            reader = new BufferedReader(new FileReader(f));
-            System.out.println("File loaded...");
-        } catch (FileNotFoundException e) {
-            System.err.println("Files not found...");
-        }
-
-        try {
-            while ((line = reader.readLine()) != null) {
-
-                if (line.length() == 0) continue;
-                splited = line.split(",");
-                Cars.add(new Car(splited[0], splited[1], splited[2], splited[3], splited[4], splited[5], splited[6]));
-            }
-        } catch (IOException e) {
-            System.out.println(e);
-
-        } catch (NullPointerException e) {
-            System.out.println(e);
-        }
-    }
-
-    //returns the entropy of the given set of data
-    public double calcEntropy(ArrayList<car> data){
-
-        if(data.size() == 0){
-            return 100000; //return something too high
-        }else{
-            //Calculate the instances of each category
-            double[] sumOfEachCategory = new double[4]; //0:unacc 1:acc 2:good 3:v-good
-
-            for(Car tempcar : data)
-            {
-                if(tempcar.getCategory().equals("unacc")) {
-                    sumOfEachCategory[0]++;
-                }else if(tempcar.getCategory().equals("acc")){
-                    sumOfEachCategory[1]++;
-                }else if(tempcar.getCategory().equals("good")){
-                    sumOfEachCategory[2]++;
-                }else if(tempcar.getCategory().equals("v-good")){
-                    sumOfEachCategory[3]++;
-                }
-            }
-
-            //We will store the probality of an instance belonging to each category
-            double [] P = new double[4];
-            for(int i = 0; i < 3; i++){
-                P[i] = sumOfEachCategory[i]/data.size();
-            }
-
-            //Entropy = ΣP(C=c)*logP(C=c)
-            double entropy = 0;
-            for(int i = 0; i < 3; i++){
-                entropy += P[i]*Math.log(P[i]);
-            }
-
-            return entropy;
-        }
-
-    }
-
-    //returns the InformationGain of this attribute
-    //remember IG(C,X) = H(C) - ΣP(X=x)*H(C|X=x) where H is entropy and C the category
-    public double calcIG(ArrayList<car> data, String attribute){
-
-        double entropy = calcEntropy(data); //This is H(C) on the type
+        return null;
+    }*/
 
 
-    }
-
-    //returns the attribute with the max InformationGain in this set of data
-    public String heuristic(ArrayList<car> data, ArrayList<String> remainingAttributes){
-
-    }
-
-    //returns a list withe the possible values of this attribute
-    public ArrayList<String> getAttributeValues(String attribute){
-
-        ArrayList<String> values = new ArrayList<>();
-        if(attribute.equals("bying") || attribute.equals("maint")){
-            values.add("low");
-            values.add("med");
-            values.add("high");
-            values.add("vhigh");
-        }else if(attribute.equals("doors")){
-            values.add("2");
-            values.add("3");
-            values.add("4");
-            values.add("5more");
-        }else if(attribute.equals("persons")){
-            values.add("2");
-            values.add("3");
-            values.add("4");
-            values.add("more");
-        }else if(attribute.equals("lug_boot")){
-            values.add("small");
-            values.add("med");
-            values.add("big");
-        }else if(attribute.equals("safety")){
-            values.add("low");
-            values.add("med");
-            values.add("high");
-        }
-        return values;
-    }
 }
--- src/Node.java
+++ src/Node.java
@@ -1,3 +1,39 @@
+import java.util.ArrayList;
+
 public class Node {
 
+    private ArrayList<Car> data;
+    private String label;
+    private boolean isLeaf;
+    private Node [] children;
+    private String attribute;
+
+    public Node(ArrayList<Car> data, String label, boolean isLeaf, Node[] children, String attribute) {
+        this.data = data;
+        this.label = label;
+        this.isLeaf = isLeaf;
+        this.children = children;
+        this.attribute = attribute;
+    }
+
+    //-----------------Getters-----------------//
+    public ArrayList<Car> getData() {
+        return data;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public boolean isLeaf() {
+        return isLeaf;
+    }
+
+    public Node[] getChildren() {
+        return children;
+    }
+
+    public String getAttribute() {
+        return attribute;
+    }
 }
